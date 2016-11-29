dec57912677ef03823c752f1be02e119fcb024fb
Krishma Araneta <krishma.araneta@smsgt.com>
2016-11-29 18:49:23 +0800

    do branding based on PGC UI, update the ff file -style.css -rate.html -history.html -myaccount.html (remove hard coded packages) -cards.html -navbar.html(remove stickershop for purchase) -redeem-card-html -purchase.html

--- public/javascripts/app/services/paypal.service.js
+++ public/javascripts/app/services/paypal.service.js
@@ -1,6 +1,6 @@
 System.register(['../utils/http-client', 'rxjs/add/operator/map', "@angular/core", "./common.service", "./user.service"], function(exports_1, context_1) {
     "use strict";
-    var __moduleName = context_1 && context_1.id;
+    var     __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
